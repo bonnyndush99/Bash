62e2465342259845f99c1c07a12bb9b9eebb343d
bonnyndush99 <bonnyndush99@gmail.com>
2025-07-09 09:41:06 -0400

    initial commit

---
+++ myfirst.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+touch bonny.txt
+
+echo "bonny">bonny.txt
+
+cat bonny.txt
+
+#program is going to sleep
+
+sleep 5
+
+echo "Oh I was asleep for 5 secs" 
+
+rm bonny.txt
+
+
---
+++ mysecondbash.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sleep 10
+
+#create a file after
+
+touch wewe.txt
+
+#sleep
+
+sleep 10
+
+echo "semaje" > wewe.txt
+
